2bc45dff6998388b60b76efe9920258f804bf97e
aaswani-v <avr9766@gmail.com>
2026-02-21 11:34:51 +0530

    goiing into more dept

--- consume_api.py
+++ consume_api.py
@@ -3,6 +3,10 @@ import json
 
 response = requests.get('http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
 for data in response.json()['items']:
-    print(data['title'])
-    print(data['link'])
+    if data['answer_count'] == 0:
+        print(data['title'])
+        print(data['link'])
+        # print()
+    else:
+        print("skipped")
     print()
