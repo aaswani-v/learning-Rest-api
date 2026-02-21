1d86e39ddd275c1c8127cec8ed0e40618d83113d
aaswani-v <avr9766@gmail.com>
2026-02-21 11:38:09 +0530

    more & more

--- consume_api.py
+++ consume_api.py
@@ -3,7 +3,8 @@ import json
 
 response = requests.get('http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
 for data in response.json()['items']:
-    if data['answer_count'] == 0:
+    if data['answer_count'] != 0:
+        # this will filter the questions which have 0 answer
         print(data['title'])
         print(data['link'])
         # print()
